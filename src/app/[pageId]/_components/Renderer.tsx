9bc8e7a465c8371ee0655c4ba6ae0b1c85fd3377
Baek JiYu <giu0213@naver.com>
2024-12-22 18:07:20 +0900

    feat: CodeBlock 추가

--- src/app/[pageId]/_components/Renderer.tsx
+++ src/app/[pageId]/_components/Renderer.tsx
@@ -2,15 +2,19 @@
 
 import React from 'react';
 
+import dynamic from 'next/dynamic';
+
 import { ExtendedRecordMap } from 'notion-types';
 import { NotionRenderer } from 'react-notion-x';
+
+const Code = dynamic(() => import('react-notion-x/build/third-party/code').then((m) => m.Code));
 
 interface Props {
   recordMap: ExtendedRecordMap;
 }
 
 function Renderer({ recordMap }: Props) {
-  return <NotionRenderer recordMap={recordMap} />;
+  return <NotionRenderer recordMap={recordMap} components={{ Code }} />;
 }
 
 export default Renderer;

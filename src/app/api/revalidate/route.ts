67bd1228396476a159be8ec5aa3f91924961f550
Baek JiYu <giu0213@naver.com>
2025-11-17 13:59:28 +0900

    feat: 모든 페이지 revalidate

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,8 +1,6 @@
 import { revalidatePath } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
-const NEED_REVALIDATE_PATH = ['/sitemap.xml'];
-
 export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams;
@@ -11,7 +9,8 @@ export async function GET(request: NextRequest) {
     if (secret !== process.env.NEXT_PUBLIC_REVALIDATE_SECRET_KEY)
       throw new Error('Failed to revalidate!');
 
-    NEED_REVALIDATE_PATH.forEach((path) => revalidatePath(path));
+    revalidatePath('/', 'layout');
+    revalidatePath('/sitemap.xml');
 
     return NextResponse.json({ revalidated: true }, { status: 200 });
   } catch (error) {

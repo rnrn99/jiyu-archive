5b2931d9314f093cfaa4616197c494de91bb076d
Baek JiYu <giu0213@naver.com>
2025-11-27 21:14:27 +0900

    refactor: styles -> app/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import 'react-notion-x/src/styles.css';
-import '../styles/globals.css';
+import './ui/globals.css';
 
 import type { Metadata } from 'next';
 
@@ -9,7 +9,7 @@ import clsx from 'clsx';
 import Header from '@/components/Header';
 import Layout from '@/components/Layout';
 
-import { notoSansKr, nanumGothicCoding } from './font';
+import { notoSansKr, nanumGothicCoding } from './ui/font';
 
 export const metadata: Metadata = {
   title: '',
